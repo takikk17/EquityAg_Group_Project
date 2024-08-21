e3476166bb6dd9dcaac7b4b280af071aacd7c300
takikk17 <takikk17@gmail.com>
2024-08-21 13:57:25 -0700

    add R files

---
+++ EquityAg_Group_Project.R
@@ -0,0 +1,9 @@
+# EQUITYAG_GROUP_PROJECT
+ 
+# created by: takikk17
+# created on: 21 August 2024
+# edited by: takikk17
+# edited on: 21 August 2024
+
+# input data from calenviroscreen40resultsdatadictionary_F_2021
+# 
